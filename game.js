fd74721d726b54cf64d7aa8ae1f1f7ae76358481
JulianRaso <51844017+JulianRaso@users.noreply.github.com>
2020-09-12 18:17:41 -0300

    V 0.5

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 var jugador = document.getElementById("character");
-var block = document.getElementById("block1");
+var block = document.getElementById("obj");
+var pad = document.getElementById("pad");
+var point = document.getElementById("point");
 var counter=0;
 
 
@@ -10,16 +12,26 @@ function jump(){
         jugador.classList.remove("animate");
     },300);
 }
+
+
+
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(jugador).getPropertyValue("top"));
+    let PointCheck = parseInt(window.getComputedStyle(point).getPropertyValue("left"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
         block.style.animation = "none";
+        pad.style.animation = "none";
         alert("Game Over. score: "+Math.floor(counter/100));
         localStorage.setItem("Player",Math.floor(counter/100));
         counter=0;
         block.style.animation = "block 1s infinite linear";
-    }else{
+        pad.style.animation = "pad 1.6s infinite linear";
+    }
+    else{
+        if(PointCheck<10 && PointCheck>-10 && characterTop<=130){
+            counter++;
+        }
         counter++;
         document.getElementById("scorelocal").innerHTML = localStorage.getItem("Player");
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
